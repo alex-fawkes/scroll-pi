9aaff58ef315c2b69621dee6c1a88461f7559bcf
Alex Fawkes <alex.fawkes@gmail.com>
2016-06-05 02:19:52 -0500

    Implement scrolling behavior and set name
    App is now scroll.pi and displays endless scrolling text of random numbers.
    Also set a dark theme with pi icon.

--- app/src/main/java/net/ddns/adfawkes/scratchapp/MainActivity.java
+++ app/src/main/java/net/ddns/adfawkes/scratchapp/MainActivity.java
@@ -1,23 +1,79 @@
 package net.ddns.adfawkes.scratchapp;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
+        new Thread(new Loop()).start();
+    }
 
+    private class Loop implements Runnable {
+        @Override
+        public void run() {
+            try {
+                loop();
+            }
+            catch (InterruptedException e) {
+                // stop
+            }
+        }
+
+        @SuppressWarnings("InfiniteLoopStatement")
+        private void loop() throws InterruptedException {
+            while (true) {
+                displayRandom();
+                Thread.sleep(10);
+            }
+        }
+
+    }
+
+    // TODO: final locals, members
+    private class Append implements Runnable {
+        private final TextView view;
+        private final String text;
+
+        public Append(TextView view, String text) {
+            this.view = view;
+            this.text = text;
+        }
+
+        @Override
+        public void run() {
+            // TODO: rename class
+            view.append(text);
+
+            // TODO: extract fn
+            final CharSequence current = view.getText();
+            final int length = current.length();
+
+            final int limit = 2000;
+            if (length > limit) {
+                final int begin = length - limit * 3 / 4;
+                view.setText(current.subSequence(begin, length));
+            }
+        }
+    }
+
+    void displayRandom() {
         View view = findViewById(R.id.textView);
         if (view instanceof TextView) {
-            TextView text = (TextView) view;
-            text.setMovementMethod(new ScrollingMovementMethod());
-            text.setText(new NativeBbpCalculator().calculateTo(10000).toPlainString());
+            // avoid threading issues entirely with local random instance
+            Random random = new Random();
+
+            StringBuilder text = new StringBuilder();
+            for (int i = 0; i < 100; ++i) {
+                text.append(String.valueOf(random.nextInt(10)));
+            }
+            view.post(new Append((TextView) view, text.toString()));
         }
     }
 
